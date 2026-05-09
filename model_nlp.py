9f76d35b0e8fef63c7906582a15b7c4372e1a129
thunder <runtosunny@163.coml>
2026-05-09 15:32:07 +0800

    cccc

--- model_nlp.py
+++ model_nlp.py
@@ -6,8 +6,11 @@ from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 from transformers import AutoModel
 from torch import nn
+from huggingface_hub import login
 
 device = torch.device("mps")
+login(token="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")
+
 
 class AFQMC(Dataset):
     def __init__(self, data_file):
@@ -39,6 +42,7 @@ class IterableAFQMC(IterableDataset):
                 yield sample
 
 
+
 checkpoint = "bert-base-chinese"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
@@ -62,25 +66,27 @@ def collote_fn(batch_samples):
 train_data = AFQMC('data/afqmc_public/train.json')
 train_dataloader = DataLoader(train_data, batch_size=4, shuffle=True, collate_fn=collote_fn)
 
+# print(train_data[0])
+
 batch_X, batch_y = next(iter(train_dataloader))
-# print('batch_X shape:', {k: v.shape for k, v in batch_X.items()})
+print('batch_X shape:', {k: v.shape for k, v in batch_X.items()})
 # print('batch_y shape:', batch_y.shape)
 # print(batch_X)
 # print(batch_y)
 
-class BertForPairwiseCLS(nn.Module):
-    def __init__(self):
-        super(BertForPairwiseCLS, self).__init__()
-        self.bert_encoder = AutoModel.from_pretrained(checkpoint)
-        self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(768, 2)
+# class BertForPairwiseCLS(nn.Module):
+#     def __init__(self):
+#         super(BertForPairwiseCLS, self).__init__()
+#         self.bert_encoder = AutoModel.from_pretrained(checkpoint)
+#         self.dropout = nn.Dropout(0.1)
+#         self.classifier = nn.Linear(768, 2)
 
-    def forward(self, x):
-        bert_output = self.bert_encoder(**x)
-        cls_vectors = bert_output.last_hidden_state[:, 0, :]
-        cls_vectors = self.dropout(cls_vectors)
-        logits = self.classifier(cls_vectors)
-        return logits
+#     def forward(self, x):
+#         bert_output = self.bert_encoder(**x)
+#         cls_vectors = bert_output.last_hidden_state[:, 0, :]
+#         cls_vectors = self.dropout(cls_vectors)
+#         logits = self.classifier(cls_vectors)
+#         return logits
 
-model = BertForPairwiseCLS().to(device)
-print(model)
+# model = BertForPairwiseCLS().to(device)
+# print(model)

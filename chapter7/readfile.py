a3dabe99ce9046a6f0a50fad2e63573a6f603904
ljl <ljl@abc.com>
2026-05-14 23:38:19 +0800

    增加课程代码

--- chapter7/readfile.py
+++ chapter7/readfile.py
@@ -6,12 +6,10 @@ from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 from transformers import AutoModel
 from torch import nn
-from huggingface_hub import login
 from pathlib import Path
 
 
 device = torch.device("mps")
-login(token="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")
 
 base_dir = Path(__file__).parent
 datafile = base_dir / '..' / 'data' / 'afqmc_public' / 'train.json'
